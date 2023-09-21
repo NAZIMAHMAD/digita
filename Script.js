f56d3e80f985e0d0f092558935ca01c48e423c9e
NAZIM AHMAD <97406116+NAZIMAHMAD@users.noreply.github.com>
2023-09-22 00:14:28 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+
+const btn=document.querySelector('.btn-primary3');
+
+btn.addEventListener("click", toggleform)
+
+
+function toggleform()
+{
+document.querySelector(".form").style.width="300px";
+}
